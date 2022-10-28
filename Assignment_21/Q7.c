b242b68e2733921352396726c9d6bc70ce14557c
Arkojeet Bera <arkojeetphy007@gmail.com>
2022-10-28 20:35:49 +0530

    Code optimized

--- Assignment_21/Q7.c
+++ Assignment_21/Q7.c
@@ -19,6 +19,47 @@ void input(struct time *time){
     scanf("%hd", &time->seconds);
 }
 
+void swap(struct time* t1, struct time* t2){
+    struct time temp = *t1;
+    *t1 =  *t2;
+    *t2 = temp;
+}
+
+struct time *diff(struct time T1, struct time T2) {
+    
+    struct time *timeDiff = (struct time *)malloc(sizeof(struct time));
+
+    if(T1.hours>T2.hours)
+        swap(&T1, &T2);
+
+    short flagSEC = 0, flagMIN = 0;
+
+    if(T2.seconds < T1.seconds){
+        timeDiff->seconds = 60 + (T2.seconds - T1.seconds);
+        flagSEC = 1;
+    }
+    else
+        timeDiff->seconds = T2.seconds - T1.seconds;
+
+    if(T2.mins < T1.mins){
+        timeDiff->mins = 60 + (T2.mins - T1.mins);
+        flagMIN = 1;
+    }
+    else
+        timeDiff->mins = T2.mins - T1.mins;
+
+    if(flagSEC)
+        timeDiff->mins--;
+
+    timeDiff->hours = T2.hours - T1.hours;
+    if(flagMIN)
+        timeDiff->hours--;
+    
+
+
+    return timeDiff;
+}
+
 int main(){
     struct time t1;
     struct time t2;
@@ -29,14 +70,11 @@ int main(){
     printf("Enter for time T2: \n");
     input(&t2);
 
-    struct time timeDiff = {
-        abs(t1.hours - t2.hours),
-        abs(t1.mins - t2.mins),
-        abs(t1.seconds - t2.seconds)};
+    struct time *timeDiff = diff(t1, t2);
 
     printf("Time difference: [%d : %d : %d] -  ", t1.hours, t1.mins, t1.seconds);
     printf("[%d : %d : %d] =  ", t2.hours, t2.mins, t2.seconds);
-    printf("[%d : %d : %d]", timeDiff.hours, timeDiff.mins, timeDiff.seconds);
+    printf("[%d : %d : %d]", timeDiff->hours, timeDiff->mins, timeDiff->seconds);
 
     return 0;
 
